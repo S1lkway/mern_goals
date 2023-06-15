6506170da20e7422fdaf01236eab823fe97647ce
Artem <silkwayway@gmail.com>
2023-06-15 15:33:09 -0400

    bcrypt and basic_register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,49 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
 
 // @desc Register user
 // @route POST /api/users
 // @access Puplic
 const registerUser = asyncHandler(async (req, res) => {
 
-  res.json({ message: 'Register user' })
+  const { name, email, password } = req.body
+
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please add all fiels')
+  }
+
+  // check User
+  const userExists = await User.findOne({ email })
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  // hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+
 })
 
 // @desc Authenticate user
