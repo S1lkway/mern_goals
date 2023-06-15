383ddef1babd50e799bfa428dcab192a27e61b2a
Artem <silkwayway@gmail.com>
2023-06-15 15:51:39 -0400

    loginUser in userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -51,7 +51,22 @@ const registerUser = asyncHandler(async (req, res) => {
 // @access Puplic
 const loginUser = asyncHandler(async (req, res) => {
 
-  res.json({ message: 'Login user' })
+  const { email, password } = req.body
+
+  // check for user mail
+  const user = await User.findOne({ email })
+
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid credentials')
+  }
 })
 
 // @desc Get user data
