83eb5b5ba2b6aced722a097b60a21ab7e08fa281
Artem <silkwayway@gmail.com>
2023-06-18 15:01:36 -0400

    add registration and logout with redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header';
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login'
 import Register from './pages/Register'
@@ -14,10 +16,10 @@ function App() {
             <Route path='/' element={<Dashboard />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
-
           </Routes>
         </div>
       </Router>
+      <ToastContainer />
     </>
   );
 }
